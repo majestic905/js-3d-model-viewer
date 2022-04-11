99b8c69ace8ef1e052083b604be8a4310d313682
Aleksandr Chilikin <majestic905@gmail.com>
2022-04-11 14:46:38 +0300

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+webpack
+sed -i -e 's/viewer\.js/viewer\.min\.js/g' dist/index.html
+firebase deploy --only hosting
+
+git restore dist/js-3d-model-viewer.min.js
+git restore dist/js-3d-model-viewer.min.js.map
+sed -i -e 's/viewer\.min\.js/viewer\.js/g' dist/index.html
