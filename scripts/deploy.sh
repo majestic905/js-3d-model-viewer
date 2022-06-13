78fff951fe69bd084fb8c4b77510b061f031382c
Aleksandr Chilikin <majestic905@gmail.com>
2022-06-13 03:31:52 +0300

    Fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@ npm run build
 sed -i -e 's/viewer\.js/viewer\.min\.js/g' dist/index.html
 firebase deploy --only hosting
 
-git restore dist/js-3d-model-viewer.min.js
-git restore dist/js-3d-model-viewer.min.js.map
+git restore dist/js/viewer.min.js
+git restore dist/js/viewer.min.js.map
 sed -i -e 's/viewer\.min\.js/viewer\.js/g' dist/index.html
